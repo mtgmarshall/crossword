d4fe9353f84156026e38bd2fe610787d610f63bc
mtgmarshall <mtgmarshall@aol.com>
2022-03-09 00:20:42 -0500

    Day 2

--- script.js
+++ script.js
@@ -1,61 +1,133 @@
-let words;                  // The user's input array of words for the crossword
-let logicArr;               // The array used for behind the scenes logic
-let gridCenter;             // The center of logicArr (also just the total word length)
+let words                   // The user's input array of words for the crossword
+let gridCenter              // The center of logicArr (also just the total word length)
+let topFiveSols = []
 let minX, maxX, minY, maxY  // (minX, minY) = top left corner
-let dispArr;                // The array used to display
-let logicIndex = 0          // Current position in the logicArr
+let dispArr                 // The array used to display
 let notReady = true
 let wordsLength
-let canvasX = 560
-let canvasY = 560
+let canvasX
+let canvasY = 720
 let squareHeight, squareWidth, textValue // drawing sizes used to draw the grid
+let numTries = 1000
+let sol
 
 function main() {
+
+//CONSIDER WHEN YOU DONT HAVE 5 SOLUTIONS
+  for (let i = 0; i < 5; i++) {
+    topFiveSols.push(new solutionState(0, 0, 0, 0, 100000000, 0, []))
+  }
+
   let input = document.getElementById("myInput").value
   words = input.split(", ")
-  words.sort(function(a, b){return b.length - a.length}) //Array of all given words in order of their sizes
+
+
+
 
   wordsLength = 0 //Determining max grid size as all words together
   for (let i = 0; i < words.length; i++) {
     wordsLength += words[i].length
   }
-
-  minX = wordsLength * 2 - 1;
-  maxX = 0;
-  minY = wordsLength * 2 - 1;
-  maxY = 0;
-
-  logicArr = new Array(wordsLength*2).fill(0).map(() => new Array(wordsLength*2).fill(0)) //Declaring the array used for behind the scenes logic
   gridCenter = wordsLength
 
-  logicIndex = 0;
+  // Start our loop through random assortments of words[] to try various solutions
+  for (let k = 0; k < numTries; k++) {
 
-  addWord(words[0], gridCenter, gridCenter, "across")
+    words.sort(function(a, b){return b.length - a.length}) //Array of all given words in order of their sizes
 
-  // Make sure all bools are checked to be ready, any false notready = true...otherwise false
-  let wordIsOrphaned = false
-  for (let i = 1; i < words.length; i++) {
-    if (!findHomeForWord(words[i])) {
-      wordIsOrphaned = true;
+    //FIX THIS SHIT
+    //words = words.sort(() => Math.random() - 0.5)
+
+    // let j, i //Shuffling the array
+    // for (i = words.length - 1; i > 0; i--) {
+    //     j = Math.floor(Math.random() * (i + 1));
+    //     [words[i], words[j]] = [words[j], words[i]]
+    // }
+
+    sol = new solutionState()
+
+    sol.minX = wordsLength * 2 - 1;
+    sol.maxX = 0;
+    sol.minY = wordsLength * 2 - 1;
+    sol.maxY = 0;
+
+    sol.logicArr = new Array(wordsLength*2).fill(0).map(() => new Array(wordsLength*2).fill(0)) //Declaring the array used for behind the scenes logic
+
+    sol.orphans = []
+
+    addWord(words[0], gridCenter, gridCenter, "across")
+
+    // Make sure all bools are checked to be ready, any false notready = true...otherwise false
+    let orphanCounter = 0 //Counts how many of the words in the array cannot be placed into logicArray
+    let numOrphans = 0;
+    for (let i = 1; i < words.length; i++) {
+      // [xxxxx, test1, orphan1, test2, orphan2] i=  1, oc = 0                0 >= 4 false
+      // [xxxxx, xxxxx, orphan1, test2, orphan2] i = 2, oc = 0                0 >= 3 false
+      // [xxxxx, xxxxx, test2, orphan2, orphan1] i--, oc++, i = 2; oc = 1     1 >= 3 false
+      // [xxxxx, xxxxx, xxxxx, orphan2, orphan1] i = 3, oc = 0                0 >= 2 false
+      // [xxxxx, xxxxx, xxxxx, orphan1, orphan2] i--, oc++, i = 3, oc = 1     1 >= 2 false
+      // [xxxxx, xxxxx, xxxxx, orphan2, orphan1] i--, oc++, i = 3, oc = 2     2 >= 2 true, so break
+      if (!findHomeForWord(words[i])) {
+        words.push(words.splice(i,1))
+
+        i--
+        orphanCounter++
+      } else {
+        orphanCounter = 0;
+      }
+
+      if (orphanCounter >= words.length - 1 - i) {
+
+        for (let j = i+1; j < words.length; j++) {
+           sol.orphans.push(words[j])
+        }
+        numOrphans = orphanCounter
+        break;
+      }
     }
-  }
 
-  //alert(minX + "  " + maxX + "  " + minY + "  " + maxY)
+    sol.fitness = pow(sol.maxX - sol.minX + 1, 2) + pow(sol.maxY - sol.minY + 1, 2) + pow((sol.maxX - sol.minX) - (sol.maxY - sol.minY), 2) * 2 + 10000 * numOrphans
+
+    for (let j = 0; j < topFiveSols.length; j++) {
+      if (sol.fitness < topFiveSols[j].fitness) {
+        //alert(sol.fitness)
+        topFiveSols.splice(j, 0, sol)
+        topFiveSols.pop()
+        break;
+      }
+    }
+
+  } // end of numTries loop
+
+  //Output in some way the orphans of the solutions
+  for (let i = 0; i < topFiveSols[0].orphans.length; i++) {
+    //alert(topFiveSols[0].orphans[i])
+  }
+  // for (let j = i+1; j < words.length; j++) {
+  //    errMessage += words[j] + "  "
+  // }
+
+  // our solution has been chosen:
+  minX = topFiveSols[0].minX
+  maxX = topFiveSols[0].maxX
+  minY = topFiveSols[0].minY
+  maxY = topFiveSols[0].maxY
 
   dispArr = new Array(maxX - minX + 1).fill(0).map(() => new Array(maxY - minY + 1).fill(0)) // <-- set up dispArr to be what we draw off of
   for (let i = 0; i <= maxX - minX; i++) { // Populates dispArr with the appropriate values from logicArr
     for (let j = 0; j <= maxY - minY; j++) {
-      dispArr[i][j] = logicArr[minX+i][minY+j]
+      dispArr[i][j] = topFiveSols[0].logicArr[minX+i][minY+j]
     }
   }
 
-  notReady = false; //wordIsOrphaned;
+  setupCanvas(); // We are ready to draw
+
 }
 
 function findHomeForWord(word) { //Determines if a given word can be placed in the logicArr and returns whether it placed the word in logicArr (will if it can)
 
-  for (let i = minX; i <= maxX; i++) { //Checking if any position has valid placement
-    for (let j = minY; j <= maxY; j++) {
+  for (let i = sol.minX; i <= sol.maxX; i++) { //Checking if any position has valid placement
+    for (let j = sol.minY; j <= sol.maxY; j++) {
 
       if (isValidPosition(word, i, j)) {
         return true;
@@ -63,42 +135,42 @@ function findHomeForWord(word) { //Determines if a given word can be placed in t
 
     }
   }
-  alert("Couldnt find a home for " + word)
   return false
 }
 
 //TEST THIS HEAVILY
 function isValidPosition(word, x, y) { //Returns whether a word can be placed at a coordinate in logicArr
 
-  if (logicArr[x][y] == 0) {
+  if (sol.logicArr[x][y] == 0) {
     return false
-  } if (!word.includes(logicArr[x][y])) {
+  } if (!word.includes(sol.logicArr[x][y])) {
     return false
   }
+
+
 
   let letterPositions = [] //Indexes in word that share the letter with logicArr[x,y]
   for (let i = 0; i < word.length; i++) {
-    if (word.charAt(i) == logicArr[x][y]) {
+    if (word.charAt(i) == sol.logicArr[x][y]) {
       letterPositions.push(i)
     }
   }
-
 
   let upLeft, downRight
 nextMatch:  for (let i = 0; i < letterPositions.length; i++){
     upLeft = letterPositions[i]                       // how far the word can go up or left
     downRight = word.length - letterPositions[i] - 1  // how far the word can go down or right
 
-    if (logicArr[x-1][y] == 0 && logicArr[x+1][y] == 0) {
+    if (sol.logicArr[x-1][y] == 0 && sol.logicArr[x+1][y] == 0) {
       for (let j = 0; j < upLeft; j++) {//Checking if placing the word left is valid
-        if (!((logicArr[x-2-j][y] == 0 || (j < upLeft-1 && logicArr[x-2-j][y] == word.charAt(upLeft-2-j))) && (((logicArr[x-1-j][y+1] == 0 && logicArr[x-1-j][y-1] == 0) || logicArr[x-1-j][y] == word.charAt(upLeft-1-j))))) {
+        if (!((sol.logicArr[x-2-j][y] == 0 || (j < upLeft-1 && sol.logicArr[x-2-j][y] == word.charAt(upLeft-2-j))) && (((sol.logicArr[x-1-j][y+1] == 0 && sol.logicArr[x-1-j][y-1] == 0) || sol.logicArr[x-1-j][y] == word.charAt(upLeft-1-j))))) {
           continue nextMatch
         }
       }
 
       for (let j = 0; j < downRight; j++) {//Checking if placing the word right is valid
         //if (!(logicArr[x+2+j][y] == 0 && logicArr[x+1+j][y+1] == 0 && logicArr[x+1+j][y-1] == 0)) {
-        if (!((logicArr[x+2+j][y] == 0 || (j < downRight-1 && logicArr[x+2+j][y] == word.charAt(letterPositions[i]+2+j))) && (((logicArr[x+1+j][y+1] == 0 && logicArr[x+1+j][y-1] == 0) || logicArr[x+1+j][y] == word.charAt(letterPositions[i]+1+j))))) {
+        if (!((sol.logicArr[x+2+j][y] == 0 || (j < downRight-1 && sol.logicArr[x+2+j][y] == word.charAt(letterPositions[i]+2+j))) && (((sol.logicArr[x+1+j][y+1] == 0 && sol.logicArr[x+1+j][y-1] == 0) || sol.logicArr[x+1+j][y] == word.charAt(letterPositions[i]+1+j))))) {
           continue nextMatch
         }
       }
@@ -106,15 +178,15 @@ nextMatch:  for (let i = 0; i < letterPositions.length; i++){
       addWord(word, x - upLeft, y, "across")
       return true
 
-    } else if (logicArr[x][y-1] == 0 && logicArr[x][y+1] == 0) {
+    } else if (sol.logicArr[x][y-1] == 0 && sol.logicArr[x][y+1] == 0) {
       for (let j = 0; j < upLeft; j++) {//Checking if placing the word up is valid
-        if (!((logicArr[x][y-2-j] == 0 || (j < upLeft-1 && logicArr[x][y-2-j] == word.charAt(upLeft-2-j))) && (((logicArr[x+1][y-1-j] == 0 && logicArr[x-1][y-1-j] == 0) || logicArr[x][y-1-j] == word.charAt(upLeft-1-j))))) {
+        if (!((sol.logicArr[x][y-2-j] == 0 || (j < upLeft-1 && sol.logicArr[x][y-2-j] == word.charAt(upLeft-2-j))) && (((sol.logicArr[x+1][y-1-j] == 0 && sol.logicArr[x-1][y-1-j] == 0) || sol.logicArr[x][y-1-j] == word.charAt(upLeft-1-j))))) {
           continue nextMatch
         }
       }
 
       for (let j = 0; j < downRight; j++) {//Checking if placing the word down is valid
-        if (!((logicArr[x][y+2+j] == 0 || (j < downRight-1 && logicArr[x][y+2+j] == word.charAt(letterPositions[i]+2+j))) && (((logicArr[x+1][y+1+j] == 0 && logicArr[x-1][y+1+j] == 0) || logicArr[x][y+1+j] == word.charAt(letterPositions[i]+1+j))))) {
+        if (!((sol.logicArr[x][y+2+j] == 0 || (j < downRight-1 && sol.logicArr[x][y+2+j] == word.charAt(letterPositions[i]+2+j))) && (((sol.logicArr[x+1][y+1+j] == 0 && sol.logicArr[x-1][y+1+j] == 0) || sol.logicArr[x][y+1+j] == word.charAt(letterPositions[i]+1+j))))) {
           continue nextMatch
         }
       }
@@ -130,6 +202,7 @@ nextMatch:  for (let i = 0; i < letterPositions.length; i++){
 }
 
 function addWord(word, x, y, dir) { //Adds word to the LogicArr at x,y going in dir, updates minX maxX minY maxY, increments logicIndex
+
   let dx, dy; //booleans to indicate across or down
   //alert(word + " is being added into the crossword at " + x + " and " + y)
   if (dir == "across") {
@@ -144,28 +217,38 @@ function addWord(word, x, y, dir) { //Adds word to the LogicArr at x,y going in 
   }
 
   for (let i = 0; i < word.length; i++) { //Adds word to the LogicArr
-    logicArr[x + i * dx][y + i * dy] = word.charAt(i)
+    try {
+      sol.logicArr[x + i * dx][y + i * dy] = word.charAt(i)
+    } catch (e) {
+      alert(e)
+    }
   }
 
-  if (x < minX) { //These four if statements update max and min used values of the LogicArray dimensions
-    minX = x
+  if (x < sol.minX) { //These four if statements update max and min used values of the LogicArray dimensions
+    sol.minX = x
   }
-  if (x + (word.length - 1) * dx > maxX) {
-    maxX = x + (word.length - 1) * dx
+  if (x + (word.length - 1) * dx > sol.maxX) {
+    sol.maxX = x + (word.length - 1) * dx
   }
-  if (y < minY) {
-    minY = y
+  if (y < sol.minY) {
+    sol.minY = y
   }
-  if (y + (word.length - 1) * dy > maxY) {
-    maxY = y + (word.length - 1) * dy
+  if (y + (word.length - 1) * dy > sol.maxY) {
+    sol.maxY = y + (word.length - 1) * dy
   }
-
-  logicIndex++; //Increment through the words array
-
 }
 
-function setup() {
+function setupCanvas() {
+
+  let numCols = maxX - minX + 1 // calculates the number of rows and columns based on the dimenions on dispArr
+  let numRows = maxY - minY + 1
+
+  canvasX = canvasY / numRows * numCols
+
   createCanvas(canvasX + 1, canvasY + 1)
+  textFont('Courier')
+
+  notReady = false;
 }
 
 function draw() {
@@ -174,14 +257,12 @@ function draw() {
   }
   background(0)
 
-  let numCols = maxX - minX + 1
+  let numCols = maxX - minX + 1 // calculates the number of rows and columns based on the dimenions on dispArr
   let numRows = maxY - minY + 1
-  squareWidth = canvasX / numCols;
+  squareWidth = canvasX / numCols; // Calculates the size of each grid for drawing
   squareHeight = canvasY / numRows;
-  textValue = min(squareWidth, squareHeight)
-  textSize(textValue)
-
-  //alert(squareWidth + "  " + squareHeight + "  " + textSize)
+  textValue = min(squareWidth, squareHeight) // Calculates the best text size to fit in the grid
+  textSize(textValue) // Sets the textSize to our calculated best text size
 
   for (let i = 0; i < numCols; i++){ //Creates the grid
     for (let j = 0; j < numRows; j++){
@@ -191,12 +272,27 @@ function draw() {
   }
 
   fill(0)
-  for (i = 0; i < numCols; i++) { //Inputs the values in logicArr into the grid
+  for (i = 0; i < numCols; i++) { //Inputs the values in dispArr into the grid
     for (j = 0; j < numRows; j++) {
       if (dispArr[i][j] != 0) {
         text(dispArr[i][j],i*squareWidth + squareWidth/2 - textValue/4, j*squareHeight + squareHeight/2 + textValue/4)
       }
+      else {
+        rect(squareWidth*i, squareHeight*j, squareWidth, squareHeight)
+      }
     }
   }
 
+}
+
+class solutionState {
+  constructor(minX, maxX, minY, maxY, fitness, logicArr, orphans) {
+    this.minX = minX
+    this.maxX = maxX
+    this.minY = minY
+    this.maxY = maxY
+    this.fitness = fitness
+    this.logicArr = logicArr
+    this.orphans = orphans
+  }
 }
